8ea414ee1fdf82688197429cffe2124722a4cf99
chrissxz <pdoce481@gmail.com>
2025-09-03 08:52:56 -0300

    Add 'caloro.py' file

---
+++ caloro.py
@@ -0,0 +1,4 @@
+nome = str(input("digite seu nome: "))
+idade = str(input("digte sua idade: "))
+altura = str(input("digite sua altura: "))
+print(f"seu nome é {nome}, você tem {idade} anos e mede {altura}")
